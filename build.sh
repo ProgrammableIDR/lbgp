44250b9a50456545c9f72af7ac6c8b4f1d85e3c9
Nicholas Hart <nic@hdb3.net>
2019-02-20 16:51:01 +0000

    build.sh fixes but still some errors i think

--- build.sh
+++ build.sh
@@ -1,1 +1,1 @@
-for D in network-simple ; bgplib bgprib bmplib zserv MRT bluster router ; do pushd $D ; cabal install --only-dependencies ; cabal configure ; cabal build ; cabal install ; popd ; done
+for D in network-simple session bgplib bgprib bmplib zserv MRT bluster router ; do pushd $D ; cabal install --only-dependencies ; cabal configure ; cabal build ; cabal install ; popd ; done
