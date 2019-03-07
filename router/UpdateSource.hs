374d15259afc7ff6c6baa26bdb98c3fa8d861bf7
Nicholas Hart <nic@hdb3.net>
2019-03-07 18:10:26 +0000

    WiP wrt UpdateSource.hs

--- router/UpdateSource.hs
+++ router/UpdateSource.hs
@@ -7,8 +7,8 @@ import BGPlib
 import BGPRib
 
 type UpdateSource = IO [ParsedUpdate]
-initSource :: PeerData -> AddrRange IPv4 -> Int -> IO UpdateSource
-initSource peer startPrefix tableSize = do
+initSource :: PeerData -> AddrRange IPv4 -> Int -> Int -> Int -> IO UpdateSource
+initSource peer startPrefix tableSize groupSize burstSize = do
     mv <- newMVar ((0,1) :: (Int,Word32))
     print $ addrRangePair startPrefix
     let (ip,l) = addrRangePair startPrefix
@@ -19,11 +19,19 @@ initSource peer startPrefix tableSize = do
              let ip4  = fromHostAddress . byteSwap32
                  ip4' = byteSwap32 . toHostAddress
                  prefix = fromAddrRange $ makeAddrRange ( ip4 ( (ip4' ip) + fromIntegral n * fromIntegral increment)) l
-                 aspath = if isExternal peer then [as] ++ [myAS $ globalData peer] else [as]
-                 localPrefOrMED = if isExternal peer then PathAttributeLocalPref 0 else PathAttributeMultiExitDisc 0
-                 nextHop = PathAttributeNextHop (localIPv4 peer)
-                 update = makeUpdate [prefix]
-                                     []
-                                     [ PathAttributeOrigin _BGP_ORIGIN_IGP , PathAttributeASPath $ ASPath4 [ASSequence aspath] , nextHop , localPrefOrMED ]
-             return update
+             return $ buildUpdate peer as [prefix]
     return (f mv)
+
+--buildPrefixes initialAddress prefixLength groupSize burstSize =
+--    map (\ip -> fromAddrRange $ makeAddrRange ( ip4 ( (ip4' ip) + fromIntegral n * fromIntegral increment)) prefixLength ips
+--    where
+--    ips = [ initialAddress + j * increment | j <- 0 .. burstSize
+
+buildUpdate peer as prefixes = let
+    aspath = if isExternal peer then [as] ++ [myAS $ globalData peer] else [as]
+    localPrefOrMED = if isExternal peer then PathAttributeLocalPref 0 else PathAttributeMultiExitDisc 0
+    nextHop = PathAttributeNextHop (localIPv4 peer)
+    in
+    makeUpdate prefixes
+               []
+               [ PathAttributeOrigin _BGP_ORIGIN_IGP , PathAttributeASPath $ ASPath4 [ASSequence aspath] , nextHop , localPrefOrMED ]
