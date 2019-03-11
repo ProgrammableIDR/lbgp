5760395b52ab891c4d40058dce820d0c37b1ca61
Nicholas Hart <nic@hdb3.net>
2019-03-11 12:36:18 +0000

    clean up coomecnted code UpdateSource.hs

--- router/UpdateSource.hs
+++ router/UpdateSource.hs
@@ -10,40 +10,7 @@ import BGPlib
 import BGPRib hiding ( group )
 
 type UpdateSource = IO [ParsedUpdate]
-{-
-initSource :: PeerData -> AddrRange IPv4 -> Int -> Int -> Int -> Int -> IO UpdateSource
-initSource peer startPrefix tableSize groupSize burstSize repeatCount = do
-    mv <- newMVar ((0,1) :: (Int,Word32))
-    print $ addrRangePair startPrefix
-    let (ip,l) = addrRangePair startPrefix
-        increment = 2 ^ (32 - l)
-        f mv = do
-             (n,as) <- takeMVar mv
-             putMVar mv $ if n == tableSize then (0,as+1) else (n + 1,as)
-             let ip4  = fromHostAddress . byteSwap32
-                 ip4' = byteSwap32 . toHostAddress
-                 prefix = fromAddrRange $ makeAddrRange ( ip4 ( (ip4' ip) + fromIntegral n * fromIntegral increment)) l
-             return $ buildUpdate peer as [prefix]
-    return (f mv)
 
---buildPrefixes initialAddress prefixLength groupSize burstSize =
---    map (\ip -> fromAddrRange $ makeAddrRange ( ip4 ( (ip4' ip) + fromIntegral n * fromIntegral increment)) prefixLength ips
---    where
---    ips = [ initialAddress + j * increment | j <- 0 .. burstSize
-
-buildUpdate peer as prefixes = let
-    aspath = if isExternal peer then [as] ++ [myAS $ globalData peer] else [as]
-    localPrefOrMED = if isExternal peer then PathAttributeLocalPref 0 else PathAttributeMultiExitDisc 0
-    nextHop = PathAttributeNextHop (localIPv4 peer)
-    in
-    makeUpdate prefixes
-               []
-               [ PathAttributeOrigin _BGP_ORIGIN_IGP , PathAttributeASPath $ ASPath4 [ASSequence aspath] , nextHop , localPrefOrMED ]
--}
-
-
-
---initSource :: AddrRange IPv4 -> Word32 -> Word32 -> IO (IO (Word32, Word32, [ParsedUpdate]))
 initSource :: PeerData -> AddrRange IPv4 -> Word32 -> Word32 -> Word32 -> IO UpdateSource
 initSource peer startPrefix tableSize groupSize burstSize = do
     mv <- newMVar  0 -- (0 :: Word32)
