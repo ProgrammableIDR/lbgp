0f096b66e493e19e4092cbb6a0a6470d58691593
Nicholas Hart <nic@hdb3.net>
2019-03-28 18:37:56 +0000

    fix clean-all.sh

--- clean-all.sh
+++ clean-all.sh
@@ -1,2 +1,2 @@
-for p in $(<pkg-dependency-order.txt) ; do echo $p ; pushd $p ; cabal clean ; rm -rf *.o *.hi ; popd ; done
 for p in $(<pkg-dependency-order.txt) ; do echo $p ; cabal-uninstall $p --force ; done
+for p in $(<pkg-dependency-order.txt) ; do echo $p ; pushd $p ; rm -rf *.o *.hi dist ; cabal clean ; popd ; done
