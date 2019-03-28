9b87b7f1d01ce27498bcc319ed2b2c2da76a488d
Nicholas Hart <nic@hdb3.net>
2019-03-28 11:53:38 +0000

    clean and simple version of CreateOpen.hs, before adding command line support

--- router/CreateOpen.hs
+++ router/CreateOpen.hs
@@ -1,18 +1,22 @@
 {-# LANGUAGE OverloadedStrings #-}
 module Main where
-import Network.Socket
-import System.IO.Error(catchIOError)
-import System.IO(IOMode( ReadWriteMode ),Handle, hClose,stdout)
+import System.IO(stdout)
 import qualified Data.ByteString.Lazy as L
-import qualified Data.ByteString as BS
 import Data.Binary(encode)
 import Data.IP
+import Data.Word
 
 import BGPlib
 
-main = do
-    L.hPut stdout $ wireFormat $ encode $ BGPOpen { myAutonomousSystem = 65000
-                     , holdTime = 180
-                     , bgpID = "192.168.122.122"
-                     , caps = [CapAS4 65000]}
-    hClose stdout
+main :: IO ()
+main = writeOpen defaultOpen
+
+writeOpen :: BGPMessage -> IO ()
+writeOpen = L.hPut stdout . wireFormat . encode
+
+defaultOpen :: BGPMessage
+defaultOpen = simpleOpen 65534 "0.0.0.0"
+
+simpleOpen :: Word32 -> IPv4 -> BGPMessage
+simpleOpen as routerID | as < 0x10000 = BGPOpen (fromIntegral as) 0 routerID [CapAS4 as]
+                       | otherwise = BGPOpen 23456 0 routerID [CapAS4 as]
