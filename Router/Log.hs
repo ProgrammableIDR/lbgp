4ba5a5cc8ec42ca35e50533c08d6e9044807ecbb
nic hart <nic@hdb3.net>
2019-06-16 12:34:00 +0100

    force flush in Log.hs

--- Router/Log.hs
+++ Router/Log.hs
@@ -14,7 +14,7 @@ log mode s = if mode >= logMode then say s else noOp s
 noOp :: String -> IO ()
 noOp _ = return ()
 say :: String -> IO ()
-say = BS.hPutStrLn outputStream . BS.pack
+say s = BS.hPutStrLn outputStream ( BS.pack s ) >> hFlush outputStream
 debug = log Debug
 trace = log Trace
 info = log Info
